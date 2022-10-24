ab8738b3a6566143392b09ac53d1d2bc70c37af5
Bart Duisters <bartduisters@bartduisters.com>
2022-10-24 19:42:48 +0200

    feat: array oefeningen en oplossingen

---
+++ array/1.oefeningen/0.js
@@ -0,0 +1,41 @@
+const persons = [
+  { name: 'John', age: 16 },
+  { name: 'Jane', age: 18 },
+  { name: 'Jack', age: 20 },
+  { name: 'Jill', age: 22 },
+  { name: 'Judy', age: 24 },
+  { name: 'Barry', age: 31 },
+];
+
+/*
+ * Gebruik de map methode om een array te maken met de namen van alle personen.
+ * Resultaat: [ 'John', 'Jane', 'Jack', 'Jill', 'Judy', 'Barry' ]
+ */
+
+/*
+ * Gebruik de map methode om een array te maken met de namen van alle personen die ouder zijn dan 18 jaar.
+ * Resultaat: [ 'Jane', 'Jack', 'Jill', 'Judy', 'Barry' ]
+ */
+
+/*
+ * Gebruik de filter methode om een array te maken met alle personen die ouder zijn dan 18 jaar.
+ * Resultaat: [ { name: 'Jane', age: 18 }, { name: 'Jack', age: 20 }, { name: 'Jill', age: 22 }, { name: 'Judy', age: 24 }, { name: 'Barry', age: 31 } ]
+ */
+
+/*
+ * Gebruik de filter methode om een array te maken met alle personen die ouder zijn dan 18 jaar en jonger dan 30 jaar.
+ * Gebruik een `chained` map methode met het resultaat van de filter methode om een array te maken met de namen van alle personen die ouder zijn dan 18 jaar en jonger dan 30 jaar.
+ * Resultaat: [ 'Jack', 'Jill', 'Judy' ]
+ */
+
+/*
+ * Gebruik de sort methode om een array te maken met de namen van alle personen alfabetisch gesorteerd.
+ * Docs: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+ * Resultaat: [ { name: 'Barry', age: 31 }, { name: 'Jack', age: 20 }, { name: 'Jane', age: 18 }, { name: 'Jill', age: 22 }, { name: 'John', age: 16 }, { name: 'Judy', age: 24 } ]
+ */
+
+/*
+ * Gebruik de sort methode om een array te maken met de namen van alle personen gesorteerd op leeftijd van oud naar jong.
+ * Resultaat: [ { name: 'Barry', age: 31 }, { name: 'Judy', age: 24 }, { name: 'Jill', age: 22 }, { name: 'Jack', age: 20 }, { name: 'Jane', age: 18 }, { name: 'John', age: 16 } ]
+ */
+const sortedPersonsByAgeDescending = persons.sort((a, b) => b.age - a.age);
---
+++ array/2.oplossingen/0.js
@@ -0,0 +1,42 @@
+const persons = [
+  { name: 'John', age: 16 },
+  { name: 'Jane', age: 18 },
+  { name: 'Jack', age: 20 },
+  { name: 'Jill', age: 22 },
+  { name: 'Judy', age: 24 },
+  { name: 'Barry', age: 31 },
+];
+
+/*
+ * Gebruik de map methode om een array te maken met de namen van alle personen.
+ */
+const names = persons.map((person) => person.name);
+
+/*
+ * Gebruik de map methode om een array te maken met de namen van alle personen die ouder zijn dan 18 jaar.
+ */
+const ages = persons.map((person) => person.age);
+
+/*
+ * Gebruik de filter methode om een array te maken met alle personen die ouder zijn dan 18 jaar.
+ */
+const adults = persons.filter((person) => person.age > 18);
+
+/*
+ * Gebruik de filter methode om een array te maken met alle personen die ouder zijn dan 18 jaar en jonger dan 30 jaar.
+ * Gebruik een `chained` map methode met het resultaat van de filter methode om een array te maken met de namen van alle personen die ouder zijn dan 18 jaar en jonger dan 30 jaar.
+ */
+const adultNamesYoungerThanThirty = persons
+  .filter((person) => person.age > 18 && person.age < 30)
+  .map((person) => person.name);
+
+/*
+ * Gebruik de sort methode om een array te maken met de namen van alle personen alfabetisch gesorteerd.
+ * Docs: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+ */
+const sortedPersonsByName = persons.sort((a, b) => a.name.localeCompare(b.name));
+
+/*
+ * Gebruik de sort methode om een array te maken met de namen van alle personen gesorteerd op leeftijd van oud naar jong.
+ */
+const sortedPersonsByAgeDescending = persons.sort((a, b) => b.age - a.age);
