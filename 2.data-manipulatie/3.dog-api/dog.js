ccb5a6c5b035b1c93bde9c103ccc375626819d81
Bart Duisters <bartduisters@bartduisters.com>
2022-11-28 21:02:21 +0100

    oplossing datamanipulatie oef 3

--- 2.data-manipulatie/3.dog-api/dog.js
+++ 2.data-manipulatie/3.dog-api/dog.js
@@ -1,19 +1,19 @@
 const getDogs = async () => {
-  /**
-   * Bezoek de url in de browser (of voer deze uit met een programma zoals Postman) om een voorbeeld te zien.
-   */
   return fetch('https://dog.ceo/api/breed/hound/images').then((response) => response.json());
 };
 
 (async () => {
   const response = await getDogs();
-  /*
-   * - Maak een nieuwe array waarin enkel afbeeldingen zitten van 'hond-blood' en log deze in de console.
-   * - Maak een nieuwe array waarin enkel het gedeelte van de string zit na "https://images.dog.ceo/breeds/" en log deze in de console.
-   */
+  const urls = response.message;
+
+  const hondBloodImages = urls.filter((url) => url.includes('hound-blood'));
+  console.log(hondBloodImages);
+
+  const subUrls = urls.map((url) => url.split('https://images.dog.ceo/breeds/')[1]);
+  console.log(subUrls);
 
   /*
-  * Extra opdracht
-  * Maak een index.html waarin de afbeeldingen getoond worden.
-  */
+   * Extra opdracht
+   * Maak een index.html waarin de afbeeldingen getoond worden.
+   */
 })();
